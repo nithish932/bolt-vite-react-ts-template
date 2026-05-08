52096e0fdc7bcf2c164601bf655d73a53a664dff
nithish932 <67693233+nithish932@users.noreply.github.com>
2026-05-08 19:24:24 +0530

    Add custom font family to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+
   plugins: [],
 }
