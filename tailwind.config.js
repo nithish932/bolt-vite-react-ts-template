19058c079a8c4c5608b9d739ab500d6ddf2aa2d8
nithish932 <67693233+nithish932@users.noreply.github.com>
2026-05-08 19:45:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-
-  theme: {
-    extend: {
-      fontFamily: {
-        display: ['Poppins', 'sans-serif'],
-      },
-    },
-  },
-
+  theme: { extend: {} },
   plugins: [],
 }
